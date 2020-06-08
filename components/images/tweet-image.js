d611130b2b62baf8a7e97099bdc0c6d302b8f337
Berkay ÇOBAN <bcobann35@gmail.com>
2020-06-09 00:42:06 +0300

    Add Tweet Image

---
+++ components/images/tweet-image.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import cn from 'classnames'
+
+import styles from './tweet-image.module.css'
+
+function TweetImage({
+  src = 'https://pbs.twimg.com/media/EaAPZ9kXQAAcs6n?format=png&name=small',
+  alt
+}) {
+  return (
+    <div className={cn([styles.tweetImage])}>
+      <img className={cn([styles.img])} src={src} alt={alt} />
+    </div>
+  )
+}
+
+export default TweetImage
---
+++ stories/4-Tweet.stories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import TweetImage from '../components/images/tweet-image'
+
+export default {
+  title: 'Tweet'
+}
+
+export const Image = () => <TweetImage />
