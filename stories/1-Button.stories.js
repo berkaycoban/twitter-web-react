88fa3d8a8e5b13430e4253b665197eceb99caaca
Berkay ÇOBAN <bcobann35@gmail.com>
2020-06-08 23:37:51 +0300

    Add ThemeButton and Stack

--- stories/1-Button.stories.js
+++ stories/1-Button.stories.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 import Button from '../components/buttons/button'
+import ThemeButton from '../components/buttons/theme-button'
 import NavigationButton from '../components/buttons/navigation-button'
 import Navigation from '../components/navigation'
 import TextTitle from '../components/typography/text-title'
+import Stack from '../components/stack'
 
 import { Home } from '../components/icons'
 
@@ -13,6 +15,15 @@ export default {
 
 export const Normal = () => <Button>Save</Button>
 
+export const Theme = () => (
+  <Stack column>
+    <ThemeButton>Save</ThemeButton>
+    <ThemeButton full>Save</ThemeButton>
+    <ThemeButton full big>
+      Save Big Button
+    </ThemeButton>
+  </Stack>
+)
 export const NavButton = () => (
   <NavigationButton>
     <Home />
