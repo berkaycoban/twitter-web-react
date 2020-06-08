b98da406f7cbfd8b21247722db476f77478a08b3
Berkay ÇOBAN <bcobann35@gmail.com>
2020-06-08 22:12:37 +0300

    Add Navigation-button and Typography

--- stories/1-Button.stories.js
+++ stories/1-Button.stories.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
 import Button from '../components/buttons/button'
+import NavigationButton from '../components/buttons/navigation-button'
+import TextTitle from '../components/typography/text-title'
+
+import { Home } from '../components/icons'
 
 export default {
   title: 'Button'
 }
 
-export const Default = () => <Button>Hello</Button>
+export const Normal = () => <Button>Save</Button>
+
+export const NavButton = () => (
+  <NavigationButton>
+    <Home />
+    <TextTitle>Home</TextTitle>
+  </NavigationButton>
+)
---
+++ stories/3-Typography.stories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import TextTitle from '../components/typography/text-title'
+
+export default {
+  title: 'Typography'
+}
+
+export const Title = () => <TextTitle>Home</TextTitle>
