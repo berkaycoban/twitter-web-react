6d9a377ad4664a7593d4883321984ea489172186
Berkay ÇOBAN <bcobann35@gmail.com>
2020-06-16 09:37:00 +0300

    Add sidebar knobs

--- stories/6-Sidebar.stories.js
+++ stories/6-Sidebar.stories.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import { withKnobs, boolean } from '@storybook/addon-knobs'
 
 import ProfileBox from '../components/sidebar/profile-box'
 import Navigation from '../components/sidebar/navigation'
 
 export default {
-  title: 'Sidebar'
+  title: 'Sidebar',
+  decorators: [withKnobs]
 }
 
 export const Profile = () => <ProfileBox />
 
-export const Nav = () => <Navigation />
+export const Nav = () => {
+  const flat = boolean('Flat', false)
+
+  return <Navigation flat={flat} />
+}
